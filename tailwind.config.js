bcd63e3a24d905b5b65224aa0d7e49b3d751af5e
Josephglz <joseph220199@gmail.com>
2024-10-11 07:37:35 -0600

    feat: Introduce custom color palette
    Defined a new custom color palette in the Tailwind configuration to establish a consistent visual identity across the application.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'accentBlue': '#08506B',
+        'primaryBlue': '#1991F0',
+        'secondaryBlue': '#0A5A99',
+        'backgroundBlue': '#E6F2FF',
+        'whiteFixed': '#FDFDFF',
+        'primaryBlueText': '#074270',
+      }
+    },
   },
   plugins: [],
 }
