3b307eec8b36099df83cdab3a50b8e43ee5ac6f8
Josephglz <joseph220199@gmail.com>
2024-10-10 21:58:35 -0600

    feat: Implement basic authentication flow
    This commit introduces an authentication system to the application.
    
    It includes the implementation of a login page, an authentication service, routing logic, and updates to the doctor service to interact with protected API endpoints.  The user's credentials are now stored locally upon successful login.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './panel/dashboard.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = { email, password };
+
+    return this.http.post<any>(environment.API_URL, body, { headers }).pipe(
+      map((response) => {
+        if (response.status === 200) {
+          this.saveCredentials(response.user as AuthModel);
+          return {
+            success: true,
+            user: response.user as AuthModel,
+            message: response.message
+          };
+        } else {
+          return {
+            success: false,
+            message: response.message
+          };
+        }
+      }),
+      catchError((error) => {
+        console.error('Error en la autenticación:', error);
+        return of({ success: false, message: 'Error en la autenticación' });
+      })
+    );
+  }
+
+  saveCredentials(user: AuthModel): void {
+    if(!user) return;
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getCredentials(): AuthModel {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
+  }
+
+  isLogged(): boolean {
+    return !!this.getCredentials();
+  }
+
+  logout(): void {
+    localStorage.removeItem('user');
+    window.location.reload();
+  }
+}
+interface AuthModel {
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at: string;
+  role: Role;
+}
+
+enum Role {
+  Doctor = 0,
+  Administrator = 1
+}
