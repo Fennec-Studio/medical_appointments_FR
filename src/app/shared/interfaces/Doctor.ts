7b8f2b3366ad932d127460572f79e936a72dfd2e
Josephglz <joseph220199@gmail.com>
2024-10-10 18:51:33 -0600

    feat: Implement DoctorService to manage doctor data
    Introduces a new service to handle the retrieval and management of doctor data within the application. This service fetches doctor information from the API and provides an observable data stream for components to access and utilize.

---
+++ src/app/shared/interfaces/Doctor.ts
@@ -0,0 +1,22 @@
+export interface Doctor {
+  id: number;
+  first_name: string;
+  last_name: string;
+  speciality_id: number;
+  room: number;
+  email: string;
+  password: string;
+  phone: string;
+  license: string;
+  created_at: Date;
+  updated_at: Date;
+  speciality?: Speciality;
+}
+
+interface Speciality {
+  id: number;
+  name: string;
+  description: string;
+  created_at: Date;
+  updated_at: Date;
+}
