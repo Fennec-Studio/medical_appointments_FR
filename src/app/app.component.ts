7e8660618edd2bd891f8a664839537afc8cc8284
Josephglz <joseph220199@gmail.com>
2024-10-10 21:59:00 -0600

    feat: Introduce navigation structure
    Replaced the standalone home view with a dedicated navigation bar component and a router outlet. This sets up the basic structure for navigating between different views in the application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './panel/views/home/home.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HomeComponent,
+    NavbarComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
