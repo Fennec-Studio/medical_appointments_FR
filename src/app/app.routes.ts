2e2c305e8ce2039bd122a3261c6e1576b2be2d19
Josephglz <joseph220199@gmail.com>
2024-10-13 13:18:41 -0600

    feat: Introduce lazy loading for improved performance
    Implements lazy loading for dashboard and appointment modules to optimize initial load time. The application will now only load these modules when they are accessed, enhancing the user experience.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,22 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './panel/dashboard.component';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
+    title: 'Inicio',
+    component: HomeComponent
+  },
+  {
+    path: 'panel',
     title: 'Dashboard',
-    component: DashboardComponent
+    loadComponent: () => import('./panel/dashboard.component').then((m) => m.DashboardComponent)
+  },
+  {
+    path: 'appointments',
+    title: 'Citas',
+    loadComponent: () => import('./pages/appointments/appointments.component').then((m) => m.AppointmentsComponent)
   },
   {
     path: 'login',
