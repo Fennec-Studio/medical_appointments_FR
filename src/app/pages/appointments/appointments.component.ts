caa1717e45d10895cadcfe21a38e5b338d9103f4
Josephglz <joseph220199@gmail.com>
2024-10-13 15:23:01 -0600

    feat: Implement appointment listing and pagination
    This provides users with a comprehensive view of appointments.
    The component retrieves appointment data, displays it in a paginated table format, and allows users to navigate through pages.

--- src/app/pages/appointments/appointments.component.ts
+++ src/app/pages/appointments/appointments.component.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { AppointmentService } from '../../services/appointment.service';
+import { AuthService } from '../../services/auth.service';
+import { Appointment } from '../../shared/interfaces/Appointment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-appointments',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './appointments.component.html'
 })
 export class AppointmentsComponent {
+  appointmentList: Appointment[] = [];
+  pages = 0;
+  pageIdx = 1;
 
+  constructor(
+    private _appointmentService: AppointmentService,
+    private _authService: AuthService
+  ) {}
+
+
+  ngOnInit() {
+    if (!this._authService.isLogged()) {
+      window.location.href = '/';
+    } else {
+      this.retrieveData();
+    }
+  }
+
+  async retrieveData() {
+    await this._appointmentService.loadAppointments()
+      .subscribe((response: Appointment[]) => {
+        this.appointmentList = response
+        this.paginationSetup();
+    })
+  }
+
+  paginationSetup() {
+    if (this.appointmentList.length === 0) {
+      this.pages = 1;
+    }
+    this.pages = Math.ceil(this.appointmentList.length / 5);
+  }
+
+  updatePageIdx(pageIdx: number) {
+    this.pageIdx = pageIdx;
+  }
+
+  showContentPage(pageIdx: number): Appointment[] {
+    this.pageIdx = pageIdx;
+    return this.appointmentList.slice((this.pageIdx - 1) * 5, this.pageIdx * 5);
+  }
 }
