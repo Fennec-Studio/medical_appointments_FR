91cc9644ca3ca9fd8b76c9b822f7ceec14f69185
Josephglz <joseph220199@gmail.com>
2024-11-09 21:38:22 -0600

    rebuild, some fixes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
